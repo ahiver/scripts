3d2fdd2b38a4a9e82f6b106dc406b7ae7c81cda1
ahiver <ihawk.bee@gmail.com>
2023-05-11 01:11:28 +0300

    fix service scripts

--- start/robot/start_camera_node.bash
+++ start/robot/start_camera_node.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-source /home/hiver/bees/scripts/env.bash
+source /home/hiver/bees/scripts/robot.env.bash
 
 # Setup camera ROS node env
 pushd $USB_CAM_DIR > /dev/null
 
 source devel/setup.bash
 
+sleep 5
 roslaunch --wait usb_cam usb_cam-test.launch
 
 popd > /dev/null
--- start/robot/start_mavros_node.bash
+++ start/robot/start_mavros_node.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-source /home/hiver/bees/scripts/env.bash
-
+source /home/hiver/bees/scripts/robot.env.bash
 rosrun mavros mavros_node _fcu_url:=${FCU_DEVICE}:${FCU_BAUD_RATE}
