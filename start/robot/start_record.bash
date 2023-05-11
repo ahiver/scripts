1c509b85179c5374a19314a5b6bf20f046db629d
ahiver <ihawk.bee@gmail.com>
2023-05-11 12:38:43 +0300

    fix experiment name in recording service

--- start/robot/start_record.bash
+++ start/robot/start_record.bash
@@ -5,6 +5,6 @@ source /home/hiver/bees/scripts/robot.env.bash
 # Setup camera ROS node env
 pushd /home/hiver/bees/scripts/record > /dev/null
 
-./record_cam_imu.bash "$(date +"%FT%H%M%S").rosbag"
+./record_cam_imu.bash "$(date +"%FT%H%M%S")"
 
 popd > /dev/null
