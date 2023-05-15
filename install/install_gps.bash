3fdd549fe84938dec40580260b7098f1b58e060e
ahiver <ihawk.bee@gmail.com>
2023-05-15 15:59:17 +0300

    add gps dependencies

--- install/install_gps.bash
+++ install/install_gps.bash
@@ -6,6 +6,7 @@ pushd ${GPS_DIR}/src > /dev/null
 git clone https://github.com/ahiver/gps
 cd ${GPS_DIR}
 rosdep install -y --from-paths src --ignore-src
+sudo apt install python3-pip
 pip3 install pyserial 
 pip3 install utm
 catkin_make
--- install/install_robot_utils.bash
+++ install/install_robot_utils.bash
@@ -5,3 +5,6 @@ sudo apt-get install htop
 
 # Machine Info
 sudo apt-get install inxi
+
+# AT modem
+sudo apt-get install minicom
