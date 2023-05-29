e1d0dcb3b5f1828f71d98071081927dae6f2cf0a
ahiver <ihawk.bee@gmail.com>
2023-05-29 18:13:21 +0300

    fix ros installation script

--- install/install_ros_raspbian.bash
+++ install/install_ros_raspbian.bash
@@ -17,9 +17,20 @@ sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool 
 # Download ROS Noetic dependency sources/repos
 sudo rosdep init && rosdep update
 mkdir ~/ros_catkin_ws && cd ~/ros_catkin_ws
-# rosinstall_generator desktop-full --rosdistro noetic --deps --wet-only --tar > noetic-ros-wet.rosinstall
-# rosinstall_generator desktop --rosdistro noetic --deps --wet-only --tar > noetic-ros-wet.rosinstall
+
+# base ROS
 rosinstall_generator ros_comm --rosdistro noetic --deps --wet-only --tar > noetic-ros-wet.rosinstall
+# base + rviz + gazebo
+# rosinstall_generator desktop --rosdistro noetic --deps --wet-only --tar > noetic-ros-wet.rosinstall
+# desktop + 3D simulation
+# rosinstall_generator desktop-full --rosdistro noetic --deps --wet-only --tar > noetic-ros-wet.rosinstall
 wstool init src noetic-ros-wet.rosinstall
 rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r --os=debian:buster
+
+# Compiling Noetic packages
+
+# use multiple cores
+sudo src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -DPYTHON_EXECUTABLE=/usr/bin/python3
+# do not use multiple cores (for limited memory)
+# sudo src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j1 -DPYTHON_EXECUTABLE=/usr/bin/python3
 
