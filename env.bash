d872f1de43f50ad1d37e1be57156e171a22909a0
ahiver <ihawk.bee@gmail.com>
2023-06-06 10:07:44 +0300

    make bee5 default configuration

--- env.bash
+++ env.bash
@@ -5,19 +5,6 @@ then
     source /opt/ros/noetic/setup.bash
 fi
 
-# Update these variables to correspond to your system setup 
-
-# export ROS_MASTER_IP=192.168.11.1
-# export ROS_MASTER_IP=192.168.1.9
-# export HOST_IP=192.168.1.9
-# #export HOST_IP=192.168.11.114
-# export HOST_NAME=uNest
-
-# export ROBOT_IP=192.168.11.1
-# export ROBOT_NAME=bee
-# End of variables to update
-
-# These parameters may be used as is
 export SCRIPTS_DIR=$WORKSPACE_DIR/scripts
 export PROJECTS_DIR=$WORKSPACE_DIR/projects
 export CONFIGS_DIR=$WORKSPACE_DIR/configs
@@ -48,4 +35,5 @@ export CAMERA_TOPIC=/usb_cam/image_raw
 export IMU_TOPIC=/mavros/imu/data_raw
 export GPS_TOPIC=/gps_data
 
-export VINS_CONFIG=bee2
+# export VINS_CONFIG=bee2
+export VINS_CONFIG=bee5
--- host.env.bash
+++ host.env.bash
@@ -13,10 +13,10 @@ source ${WORKSPACE_DIR}/scripts/env.bash
 # uncomment if robot is master
 MASTER_IP=192.168.11.1
 # uncomment if dev machine is master
-# MASTER_IP=127.0.01
+# MASTER_IP=127.0.0.1
 
 
 
 export ROS_MASTER_URI=http://${MASTER_IP}:11311
-export ROS_IP=127.0.0.1 #$HOST_IP
+export ROS_IP=192.168.11.114 #127.0.0.1 #$HOST_IP
 export ROS_HOSTNAME=uNest #$HOST_NAME
