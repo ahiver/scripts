3df9f240f268ba65fd8c5e5c42da30d9f5293383
ahiver <ihawk.bee@gmail.com>
2023-06-08 17:16:14 +0300

    stat camera: wait for ros service

--- start/robot/start_camera_node.bash
+++ start/robot/start_camera_node.bash
@@ -7,8 +7,8 @@ pushd ${USB_CAM_DIR} > /dev/null
 
 source devel/setup.bash
 
-# roslaunch --wait usb_cam usb_cam-test.launch
-roslaunch usb_cam usb_cam-test.launch
+roslaunch --wait usb_cam usb_cam-test.launch
+# roslaunch usb_cam usb_cam-test.launch
 
 popd > /dev/null
 
