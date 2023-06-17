0e7102268a4d1bb5bee4e07183f0c6da52f2c729
ahiver <ihawk.bee@gmail.com>
2023-06-17 17:40:26 +0300

    run mavproxy as root service

--- install/install_mavproxy.bash
+++ install/install_mavproxy.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get install python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame
-sudo pip3 install PyYAML mavproxy
-# echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
+pip3 install PyYAML mavproxy
+echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
+# sudo pip3 install PyYAML mavproxy
