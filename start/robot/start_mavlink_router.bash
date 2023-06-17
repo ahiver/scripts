b7e72f7fa3b38771b52a478ed0f88289db179b8d
ahiver <ihawk.bee@gmail.com>
2023-06-17 23:22:30 +0300

    mavlink_router configuration

--- start/robot/start_mavlink_router.bash
+++ start/robot/start_mavlink_router.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# source ${ENV_SCRIPT}
-
-mavlink-routerd -e 0.0.0.0:14550 -e 127.0.0.1:14540 /dev/ttyACM0:921600
+source ${ENV_SCRIPT}
+# mavlink-routerd -e 127.0.0.1:14541 -e 127.0.0.1:14540 ${FCU_DEVICE}:${FCU_BAUD_RATE} --log
+# logs in /var/log/flight-stack
+# tcp port 5760
+# mavlink-routerd -e 127.0.0.1:14550 -e 127.0.0.1:14540 ${FCU_DEVICE}:${FCU_BAUD_RATE} --log ${LOGS_DIR}
+mavlink-routerd --conf-file ${SCRIPTS_DIR}/mavlink_router.conf --log ${LOGS_DIR}
