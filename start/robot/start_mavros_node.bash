738755288edc67af9a14523cc7a97833a4c1b826
ahiver <ihawk.bee@gmail.com>
2023-06-17 16:16:21 +0300

    use udp from mavproxy for mavros_node

--- start/robot/start_mavros_node.bash
+++ start/robot/start_mavros_node.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source ${ENV_SCRIPT}
-rosrun mavros mavros_node _fcu_url:=${FCU_DEVICE}:${FCU_BAUD_RATE}
+# rosrun mavros mavros_node _fcu_url:=${FCU_DEVICE}:${FCU_BAUD_RATE}
+rosrun mavros mavros_node _fcu_url:=udp://@localhost
