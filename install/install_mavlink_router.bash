84d1c43f874ed0f16182e8bf05b5c25113dcfcd6
ahiver <ihawk.bee@gmail.com>
2023-06-17 21:25:09 +0300

    fixes for mavlink_router scripts

--- install/install_mavlink_router.bash
+++ install/install_mavlink_router.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p ${MAVLINK_ROUTER_DIR}
 pushd ${MAVLINK_ROUTER_DIR} > /dev/null
 
 # download the source 
@@ -10,7 +11,9 @@ sudo apt install git meson ninja-build pkg-config gcc g++ systemd
 
 # build and install
 sudo apt install python3-pip
-sudo pip3 install meson
+# sudo pip3 install --user meson
+# meson.build:1:0: ERROR: Meson version is 0.53.2 but project requires >= 0.55
+sudo pip3 install meson==0.55 --ignore-installed
 meson setup build .
 ninja -C build
 
