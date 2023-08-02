e99c4ac59b53d526d83ad436d5d5027f5289278d
ahiver <ihawk.bee@gmail.com>
2023-08-02 17:30:12 +0300

    fix install_mavros script

--- install/install_mavros_ubuntu.bash
+++ install/install_mavros_ubuntu.bash
@@ -8,7 +8,7 @@ pushd ${TMP_DIR} > /dev/null
 sudo apt-get install ros-noetic-mavros ros-noetic-mavros-extras
 wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
 chmod a+x install_geographiclib_datasets.sh
-./install_geographiclib_datasets.sh
+sudo ./install_geographiclib_datasets.sh
 
 popd > /dev/null
 rm -rf ${TMP_DIR}
