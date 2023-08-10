99be9279827256c2c6f3400f18b1c0eec8a47b8c
ahiver <ihawk.bee@gmail.com>
2023-08-10 11:00:29 +0300

    add vin start on load

--- start/robot/post_setup.bash
+++ start/robot/post_setup.bash
@@ -6,3 +6,13 @@ sleep 10
 
 # set imu mavlink connection rate to 300 hz
 rosrun mavros mavsys rate --stream-id 1 300
+
+sleep 10
+
+# start vins
+
+pushd ${PROJECTS_DIR}/vins > /dev/null
+
+vins/start_vins.bash
+
+popd > /dev/null
