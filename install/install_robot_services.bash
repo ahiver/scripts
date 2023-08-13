1f9df8ef6308470c0cbb7385c99fa569b82efce7
ahiver <ihawk.bee@gmail.com>
2023-08-13 21:07:51 +0300

    rename mavproxy and record services

--- install/install_robot_services.bash
+++ install/install_robot_services.bash
@@ -10,12 +10,12 @@ sudo cp -rf *.service /etc/systemd/system
 popd > /dev/null
 
 sudo systemctl daemon-reload
-sudo systemctl enable mavproxy.service
+sudo systemctl enable mavproxy2.service
 sudo systemctl enable ros.service
 sudo systemctl enable camera.service
 sudo systemctl enable mavros.service
 sudo systemctl enable post_setup.service
 # sudo systemctl enable mavlink_router.service
 # sudo systemctl enable gps.service
-sudo systemctl enable record.service
+sudo systemctl enable record2.service
 
