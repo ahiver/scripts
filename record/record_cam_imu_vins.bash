a7d4859a97dd63ff60a6da5dfa1ba00f5ca9a3cf
ahiver <ihawk.bee@gmail.com>
2023-08-14 11:54:25 +0300

    add vins bag recording

---
+++ record/record_cam_imu_vins.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+      echo "Experiment name is not defined."
+      exit 0
+fi
+
+EXPERIMENT_NAME=$1
+BAG_DIR=$BAGS_DIR/$EXPERIMENT_NAME
+mkdir -p $BAG_DIR
+
+pushd ${BAG_DIR} > /dev/null
+ 
+BAG_NAME=cam_imu_vins.bag
+BAG_PATH=$BAG_DIR/$BAG_NAME
+echo Output path: ${BAG_PATH}
+echo Press Ctrl+C to finish.
+rosbag record --lz4 --output-name=$BAG_NAME $CAMERA_TOPIC $IMU_TOPIC $VINS_TOPICS __name:=bag_recording
+
+popd > /dev/null
