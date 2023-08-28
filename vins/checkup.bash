d0b6fe6d8d1ce4f43ac99b5eae42ff0904b2a569
ahiver <ihawk.bee@gmail.com>
2023-08-28 20:57:18 +0300

    improve checkup script

--- vins/checkup.bash
+++ vins/checkup.bash
@@ -6,7 +6,13 @@ source ../robot.env.bash
  
 rostopic list
 
-rostopic echo -n1 /usb_cam/image_raw
-rostopic echo -n1 mavros/imu/data_raw
+rostopic hz /usb_cam/image_raw
+
+# rostopic echo -n1 /usb_cam/image_raw
+# rostopic echo -n1 mavros/imu/data_raw
 
 uvcdynctrl --device=video0 --get=Exposure
+
+rosrun mavros mavsys rate --stream-id 1 300
+
+rostopic hz /mavros/imu/data_raw
