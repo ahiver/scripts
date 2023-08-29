36d3745b86358351790efd5de8a0bc10eced577b
ahiver <ihawk.bee@gmail.com>
2023-08-29 06:02:28 +0300

    use usb_cam node for camera

--- start/robot/start_camera_node.bash
+++ start/robot/start_camera_node.bash
@@ -3,12 +3,12 @@
 source ${ENV_SCRIPT}
 
 # # Setup camera ROS node env
-# pushd ${USB_CAM_DIR} > /dev/null
+pushd ${USB_CAM_DIR} > /dev/null
 
-# source devel/setup.bash
+source devel/setup.bash
 
-# roslaunch --wait usb_cam usb_cam-test.launch
+roslaunch --wait usb_cam usb_cam-test.launch
 # # roslaunch usb_cam usb_cam-test.launch
-# popd > /dev/null
+popd > /dev/null
 
-roslaunch --wait video_stream_opencv camera.launch camera_name:=usb_cam
+#roslaunch --wait video_stream_opencv camera.launch camera_name:=usb_cam
