0597fa35914ad1e858ce65deac274a07441cbc57
ahiver <ihawk.bee@gmail.com>
2023-09-01 23:24:08 +0300

    setup fix

--- vins/setup.bash
+++ vins/setup.bash
@@ -17,7 +17,7 @@ rosrun mavros mavsys rate --stream-id 1 300
 
 rostopic hz /mavros/imu/data_raw
 
-pushd ${OPEN_VINS_DIR}/src > /dev/null
+pushd ${OPEN_VINS_DIR} > /dev/null
 source devel/setup.bash
 popd > /dev/null
 
