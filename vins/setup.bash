dcd083e53c3132d901744e48eb0364f19a78e9b5
ahiver <ihawk.bee@gmail.com>
2023-09-01 23:12:59 +0300

    set_origin on setup

--- vins/setup.bash
+++ vins/setup.bash
@@ -17,4 +17,8 @@ rosrun mavros mavsys rate --stream-id 1 300
 
 rostopic hz /mavros/imu/data_raw
 
+pushd ${OPEN_VINS_DIR}/src > /dev/null
+source devel/setup.bash
+popd > /dev/null
+
 rosrun ov_msckf set_origin.py
