571c7ed11b033dab631be4b19d40ee57b3619b63
ahiver <ihawk.bee@gmail.com>
2023-09-26 17:48:35 +0300

    calibrate_imu change path for allan config flie

--- calibrate/calibrate_imu.bash
+++ calibrate/calibrate_imu.bash
@@ -10,12 +10,11 @@ EXPERIMENT_NAME=$1
 BAG_DIR=$BAGS_DIR/$EXPERIMENT_NAME
 BAG_NAME=imu.bag
 ALLAN_CONFIG_FILE=imu_allan.yaml
-ALLAN_CONFIG_PATH=configs/${ALLAN_CONFIG_FILE}
+ALLAN_CONFIG_PATH=$(pwd)/configs/${ALLAN_CONFIG_FILE}
 # ALLAN_CONFIG_PATH=$BAG_DIR/$ALLAN_CONFIG_FILE
 BAG_PATH=$BAG_DIR/$BAG_NAME
 OUTPUT_DIR=$CALIBRATIONS_DIR/${EXPERIMENT_NAME}/imu
 COOKED_BAG_PATH=${OUTPUT_DIR}/cooked_${BAG_NAME}
-
 if [ ! -f "$ALLAN_CONFIG_PATH" ]
 then
     echo "$ALLAN_CONFIG_PATH does not exist."
@@ -25,7 +24,7 @@ fi
 mkdir -p ${OUTPUT_DIR}
 source $ALLAN_VARIANCE_DIR/devel/setup.bash
 pushd $OUTPUT_DIR > /dev/null
-rosrun allan_variance_ros cookbag.py --input ${BAG_PATH} --output ${COOKED_BAG_PATH}
+# rosrun allan_variance_ros cookbag.py --input ${BAG_PATH} --output ${COOKED_BAG_PATH}
 rosrun allan_variance_ros allan_variance . ${ALLAN_CONFIG_PATH}
 rosrun allan_variance_ros analysis.py --data allan_variance.csv --config ${ALLAN_CONFIG_PATH}
 popd > /dev/null
