cb1edd64de25393de053cef92f0cc4f1a05a6e0f
ahiver <ihawk.bee@gmail.com>
2023-09-27 21:21:19 +0300

    add dependency to usb_cam

--- install/install_usb_cam.bash
+++ install/install_usb_cam.bash
@@ -7,6 +7,7 @@ sudo apt-get install protobuf-compiler libprotobuf-dev
 sudo apt install python3-rosdep2
 sudo apt-get install ros-noetic-image-common
 sudo apt install v4l-utils
+apt-get install libavcodec-dev
 
 # ROS Camera node for ov7251
 mkdir -p ${USB_CAM_DIR}/src
